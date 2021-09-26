8400387a9540436a9c93d339b7e77fdbd15defd9
Tom Maenan Read Cutting <readcuttingt@gmail.com>
2021-09-26 16:24:23 +0100

    Implement initial gnu string table parsing

--- src/archive/Archive.zig
+++ src/archive/Archive.zig
@@ -45,10 +45,44 @@ pub fn parse(self: *Archive, allocator: *Allocator, stderr: anytype) !void {
         }
     }
 
-    // TODO: Identify string table and other cross-platform shenanigans! here
     // https://www.freebsd.org/cgi/man.cgi?query=ar&sektion=5
+    // Process string/symbol tables and/or try to infer archive type!
+    {
+        var starting_seek_pos = format.magic_string.len;
 
-    _ = allocator;
+        var first_line_buffer: [60]u8 = undefined;
+
+        const has_line_to_process = result: {
+            const chars_read = reader.read(&first_line_buffer) catch |err| switch (err) {
+                else => |e| return e,
+            };
+
+            if (chars_read < first_line_buffer.len) {
+                break :result false;
+            }
+
+            break :result true;
+        };
+
+        if (has_line_to_process) {
+            if (first_line_buffer[0..1] == "//"[0..1]) {
+                switch (self.archive_type) {
+                    .ambiguous => self.archive_type = .gnu,
+                    .gnu, .gnu64 => {},
+                    else => {
+                        try stderr.print("Came across gnu-style string table in {} archive\n", .{self.archive_type});
+                        return error.NotArchive;
+                    },
+                }
+
+                const table_size_string = first_line_buffer[48..58];
+                const table_size = try fmt.parseInt(u32, mem.trim(u8, table_size_string, " "), 10);
+                starting_seek_pos = starting_seek_pos + first_line_buffer.len + table_size;
+            }
+        }
+
+        try reader.context.seekTo(starting_seek_pos);
+    }
 
     while (true) {
         const archive_header = reader.readStruct(format.ar_hdr) catch |err| switch (err) {
