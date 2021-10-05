bd4cde4bdc73eb7c221b494e6a005b6e16f87b84
iddev5 <ayushtechportal@gmail.com>
2021-10-05 21:40:53 +0530

    Generate symbol table in even number sizes for gnu style.
    This is a strange behavior commonly followed by llvm-ar and binutils-ar

--- src/archive/Archive.zig
+++ src/archive/Archive.zig
@@ -144,7 +144,6 @@ pub fn finalize(self: *Archive, allocator: *Allocator) !void {
     switch (self.output_archive_type) {
         .gnu, .gnuthin, .gnu64 => {
             // GNU format: Create string table
-            var string_table_entries: usize = 0;
             var string_table = std.ArrayList(u8).init(allocator);
             defer string_table.deinit();
 
@@ -162,8 +161,6 @@ pub fn finalize(self: *Archive, allocator: *Allocator) !void {
                     try string_table.appendSlice(file.name);
                     try string_table.appendSlice("/\n");
 
-                    string_table_entries += 1;
-
                     break :blk pos;
                 }});
                 defer allocator.free(name);
@@ -175,7 +172,7 @@ pub fn finalize(self: *Archive, allocator: *Allocator) !void {
             // Write the string table itself
             {
                 if (string_table.items.len != 0) {
-                    if (string_table_entries == 1)
+                    if (string_table.items.len % 2 != 0)
                         try string_table.append('\n');
                     try writer.print("//{s}{: <10}`\n{s}", .{ " " ** 46, string_table.items.len, string_table.items });
                 }
